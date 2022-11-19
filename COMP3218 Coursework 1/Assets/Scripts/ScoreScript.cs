9ba20eab3fb8c8c89cefef636548db10c3231ce2
Christos Mousoulides <cm10g19@soton.ac.uk>
2022-11-19 16:29:00 +0000

    Added Scoring that remains between scenes

---
+++ COMP3218 Coursework 1/Assets/Scripts/ScoreScript.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+ using UnityEngine.SceneManagement;
+
+public class ScoreScript : MonoBehaviour
+{
+    public Text scoreText;
+    public int score;
+
+    private float timer = 0f;
+    public float delayAmount;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Scene currentScene = SceneManager.GetActiveScene ();
+        string sceneName = currentScene.name;
+        if(sceneName == "Level1"){
+        score = 0;
+        }
+        else{
+        score = StateScoreController.globalScore;
+        }
+
+        scoreText.text = "Score: " + score;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer += Time.deltaTime;
+ 
+    if (timer >= delayAmount)
+    {
+       timer = 0f;
+       score++;
+       scoreText.text = "Score: " + score;
+       StateScoreController.globalScore = score;
+    }
+    }
+}
