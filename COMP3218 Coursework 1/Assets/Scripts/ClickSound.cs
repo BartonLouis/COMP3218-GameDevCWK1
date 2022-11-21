a173726532be86f88fe585fdd556c6e3cab001d3
BartonLouis <louis.barton@outlook.com>
2022-11-21 13:13:00 +0000

    Bug fixes

--- COMP3218 Coursework 1/Assets/Scripts/ClickSound.cs
+++ COMP3218 Coursework 1/Assets/Scripts/ClickSound.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class ClickSound : MonoBehaviour
 {
-     [SerializeField] private AudioSource clickSoundEffect;
+     [SerializeField] public AudioSource clickSoundEffect;
     // Start is called before the first frame update
     public void playSound(){
     
